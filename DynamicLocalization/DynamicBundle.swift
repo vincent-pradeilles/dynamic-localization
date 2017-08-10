08b3e20d734ee29c98b6bafe9aac9648ff8fd9f3
Vincent Pradeilles <little.facto@gmail.com>
2017-08-10 21:33:16 +0200

    Use a more encapsulated API

--- DynamicLocalization/DynamicBundle.swift
+++ DynamicLocalization/DynamicBundle.swift
@@ -8,12 +8,22 @@
 
 import Foundation
 
-private var kBundleKey = 0
-
 class DynamicBundle: Bundle {
     
+    static private var kCurrentLocalizationBundleKey = 0
+    
+    fileprivate var currentLocalizationBundle: Bundle? {
+        get {
+            return objc_getAssociatedObject(self, &DynamicBundle.kCurrentLocalizationBundleKey) as? Bundle
+        }
+        
+        set {
+            objc_setAssociatedObject(self, &DynamicBundle.kCurrentLocalizationBundleKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
     override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
-        if let bundle = objc_getAssociatedObject(self, &kBundleKey) as? Bundle {
+        if let bundle =  self.currentLocalizationBundle {
             return bundle.localizedString(forKey: key, value: value, table: tableName)
         } else {
             return super.localizedString(forKey: key, value: value, table: tableName)
@@ -28,9 +38,9 @@ extension Bundle {
     class func setLanguage(_ language: String) {
         object_setClass(Bundle.main, DynamicBundle.self)
         
-        if let localizationBundlePath = Bundle.main.path(forResource: language, ofType: "lproj"),
-            let localizationBundle = Bundle(path: localizationBundlePath) {
-            objc_setAssociatedObject(Bundle.main, &kBundleKey, localizationBundle, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        if let newLocalizationBundlePath = Bundle.main.path(forResource: language, ofType: "lproj"),
+            let newLocalizationBundle = Bundle(path: newLocalizationBundlePath) {
+            (Bundle.main as? DynamicBundle)?.currentLocalizationBundle = newLocalizationBundle
             NotificationCenter.default.post(name: LocalizationWasModifiedNotification, object: nil)
         }
     }
